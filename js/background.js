7dec33e44f11e2135eb793e6adf13fb863161836
Alexey Pegov <pegov@almworks.com>
2017-10-26 13:55:04 +0300

    version 1.0.4: open page in new tab

--- js/background.js
+++ js/background.js
@@ -140,7 +140,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.storage.local.get(SERVER_KEY, function(values) {
     const url = values[SERVER_KEY];
     if (url && url.trim().length > 0) {
-      chrome.tabs.update(tab.id, {url: url});
+      chrome.tabs.create({url: url});
     }
   });
 });
